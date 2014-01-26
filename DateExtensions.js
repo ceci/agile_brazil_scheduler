70eef4f7c85c3542effeac369c5adb82ddacdfe0
Ceci Fernandes <ceci.vidageek@gmail.com>
2014-01-26 04:33:40 -0200

    And now timespan (aka rowspan) works too!

--- DateExtensions.js
+++ DateExtensions.js
@@ -7,3 +7,12 @@ Date.prototype.dateToJSON = function() {
 Date.prototype.timeToJSON = function() {
 	return this.toLocaleTimeString().substring(0,5);
 };
+Date.prototype.halfHoursUntil = function(otherTime) {
+	var hour = new Date(this);
+	var numberOfHalfHours = 0;
+	while (hour < otherTime) {
+		numberOfHalfHours++;
+		hour.addHalfHour();
+	}
+	return numberOfHalfHours;
+};
--- Session.js
+++ Session.js
@@ -2,29 +2,43 @@ function Session(session) {
 	this.id = session.id;
 	this.room = session.room_id;
 	this.roomspan = session.roomspan;
-	if (session.detail_type == "AllHands"){
+	if (session.detail_type == "AllHands")
 		this.roomspan = 6;
-	}
+
 	if (session.start_at)
 		this.startTime = new Date(session.start_at);
 	if (session.end_at)
 		this.endTime = new Date(session.end_at);
+	this.timespan = this.startTime.halfHoursUntil(this.endTime);
 
 	this.allocated = function() {
 		return this.startTime && this.endTime;
 	}
 
 	this.positionYourself = function() {
+		var time = new Date(this.startTime.toJSON());
 		var td = $("[data-room='" + this.room + "']" +
-					"[data-time='" + this.startTime.toJSON() + "']")
-					.attr("colspan", this.roomspan);
+					"[data-time='" + time.toJSON() + "']")
+					.attr("colspan", this.roomspan)
+					.attr("rowspan", this.timespan);
 		td.text(this.id);
+		removeCells(this.roomspan - 1, this.room + 1, time);
+		time.addHalfHour();
+		for (var i = 1; i < this.timespan; i++) {
+			removeCells(this.roomspan, this.room, time);
+			time.addHalfHour();
+		}
+	}
 
-		var cellToRemove = (this.roomspan - 1) + this.room;
-		while (cellToRemove > this.room) {
-			$("[data-room='" + cellToRemove + "']" +
-				"[data-time='" + this.startTime.toJSON() + "']").remove();
-			cellToRemove--;
+	function removeCells(howMany, actualRoom, time) {
+		var roomToBeRemoved = howMany + actualRoom - 1;
+		while (roomToBeRemoved >=	 actualRoom) {
+			console.log("=== Removing cell " + time + " from " + roomToBeRemoved);
+			var cell = $("[data-room='" + roomToBeRemoved + "']" +
+				"[data-time='" + time.toJSON() + "']");
+			console.log(cell);
+			cell.remove();
+			roomToBeRemoved--;
 		}
 	}
 }
